0aa1554fff3f6167d44972fc3afe4f6d180ffe57
bitmotte <bitmotte@proton.me>
2026-02-11 09:07:16 -0500

    audio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{fs::File, io::BufReader};
+use rodio::{Decoder, OutputStreamBuilder, Sink};
+
 fn main() {
-    println!("Hello, world!");
+    let stream_handler = OutputStreamBuilder::open_default_stream().expect("no stream");
+    let sink = Sink::connect_new(&stream_handler.mixer());
+
+    let file = File::open("audio/test.ogg").unwrap();
+    let source = Decoder::new(BufReader::new(file)).unwrap();
+
+    sink.append(source);
+    sink.sleep_until_end();
 }
